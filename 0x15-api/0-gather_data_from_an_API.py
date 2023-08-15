9d1dedd843ef67b1b7d4424450c501aa4708d53d
TeddyMuli <tmuli974@gmail.com>
2023-08-15 14:02:35 +0300

    pycode

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,27 +6,28 @@ import requests
 from requests import get
 from sys import argv
 
+
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = f"{base_url}/users/{employee_id}"
     todos_url = f"{base_url}/todos?userId={employee_id}"
 
     try:
-        user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
+        user_r = requests.get(user_url)
+        todos_r = requests.get(todos_url)
 
-        if user_response.status_code != 200 or todos_response.status_code != 200:
+        if user_r.status_code != 200 or todos_r.status_code != 200:
             print("Error: Unable to fetch data from the API.")
             return
 
-        user_data = user_response.json()
-        todos_data = todos_response.json()
+        user_data = user_r.json()
+        todos_data = todos_r.json()
 
-        employee_name = user_data['name']
-        total_tasks = len(todos_data)
-        done_tasks = sum(1 for task in todos_data if task['completed'])
+        emp_name = user_data['name']
+        total = len(todos_data)
+        done = sum(1 for task in todos_data if task['completed'])
 
-        print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+        print(f"Employee {emp_name} is done with tasks({done}/{total}):")
 
         for task in todos_data:
             if task['completed']:
@@ -34,6 +35,7 @@ def get_employee_todo_progress(employee_id):
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
+
 
 if __name__ == "__main__":
     employee_id = int(input("Enter the employee ID: "))
