64a7c55c29b0f11105a1eea7bd9315dc109b4ebc
TeddyMuli <tmuli974@gmail.com>
2024-04-15 20:23:09 +0300

    added comments

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Installs flask
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip']
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
