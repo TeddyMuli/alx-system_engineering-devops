d0fb8e2e236740d893c202c02ef99e0bbe260964
TeddyMuli <tmuli974@gmail.com>
2024-08-26 21:11:54 +0300

    fix 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""returns the number of subscribers of a subreddit
-"""
-from requests import get
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """ returns the numbers of subscribers
-    """
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
-                Gecko/20100101 Firefox/108.0"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = get(url, headers=headers).json()
-    subscribers = response.get('data', {}).get('subscribers', 0)
-    return subscribers
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
